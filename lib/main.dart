e99a1073ac1c8ade466c13dd9f37da01730cfc7a
Hasnain17 <hasnaingakhar1998@gmail.com>
2024-04-30 16:57:05 +0500

    Task Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,63 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+      MyFirstApp()
   );
 }
+
+
+
+class MyFirstApp extends StatelessWidget {
+  const MyFirstApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+
+              Container(
+                width: 100.0,
+                height: double.infinity,
+                margin: EdgeInsets.only(bottom: 40.0),
+                color: Colors.red,
+              ),
+
+              Container(
+                width: 100.0,
+                height: 200.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+
+              Container(
+                width: 100.0,
+                height: double.infinity,
+                margin: EdgeInsets.only(bottom: 40.0),
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
